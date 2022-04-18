b8a9faf198140216cda1907fe26729e93778a2fd
Emerson Estrella <emerson.estrella@gmail.com>
2022-04-19 00:21:56 +0200

    Replacing drawer per tabs as main navigation method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { MDCRipple } from '@material/ripple';
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCDrawer } from "@material/drawer";
 import { MDCList } from '@material/list';
+import {MDCTabBar} from '@material/tab-bar';
 // import {MDCTextField} from '@material/textfield';
 // import {MDCDialog} from '@material/dialog';
 // import {MDCChipSet} from '@material/chips';
@@ -12,45 +13,20 @@ const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
 });
 
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+drawer.open = false;
+
+const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
 });
 
+// document.querySelector('.mdc-drawer .mdc-list').addEventListener('click', (event) => {
+//   drawer.open = false;
+// });
+
+// const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
 const list = new MDCList(document.querySelector('.mdc-deprecated-list'));
 list.wrapFocus = true;
 const listItemRipples = list.listElements.map((listItemEl) => new MDCRipple(listItemEl));
-
-// const textFields = [].map.call(document.querySelectorAll('.mdc-text-field'), function (el) {
-//     return new MDCTextField(el);
-// });
-
-const
-  screen = {
-    small: 0,
-    medium: 960,
-    large: 1280
-  };
-
-// observe window resize
-window.addEventListener('resize', resizeHandler);
-
-// initial call
-resizeHandler();
-
-// calculate size
-function resizeHandler() {
-  // get window width
-  const iw = window.innerWidth; 
-  // determine named size
-  let size = null;
-  for (let s in screen) {
-    if (iw >= screen[s]) size = s;
-  }
-  if(size == "large") {
-    drawer.open = true;
-  } else {
-    drawer.open = false;
-  }
-}
